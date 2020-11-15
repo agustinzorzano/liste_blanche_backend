77d8432ab2546999af6317b035a19fa1dcabeebc
agustin <aguszorza@gmail.com>
2020-11-15 15:16:03 +0100

    Refactor: move the email analysis logic to a class

--- email_scan.py
+++ email_scan.py
@@ -1,93 +1,16 @@
 import sys
 import os
-import logging
-import re
 from spam import db
 from spam.imap import Imap
-from spam.models import User, WhiteList, Quarantine, WhiteListRegularExpression, BlackListRegularExpression, BlackList
+from spam.models import User, Quarantine
 from spam.smtp import Smtp
 from spam.email import Email
-from spam.message_creator import MessageCreator
+from spam.email_analyzer import EmailAnalyzer
 from dotenv import load_dotenv
 load_dotenv()
 
 BASE_PATH = os.environ.get("BASE_PATH")
-TEMPLATE_NAME = 'captcha_email.html'
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-
-def fulfils_expression(mail, expressions):
-    """It returns True if the email address matches one of the regular expressions in the expressions list"""
-    for expression in expressions:
-        if re.fullmatch(expression, mail):
-            return True
-    return False
-
-
-def analyse_mails(mailbox, smtp_sender, white_list, mails, is_unseen, user):
-    """It checks the sender for each email from the list. If the sender is not in the white list it will do one of the
-    following actions:
-        - If the email is in the black list, it will delete it from the mailbox.
-        - If the email is not in the black list, it will delete it from the mailbox, save it in a repository as an .eml file,
-          save some specific data in the database and send an email to the sender with the captcha.
-    """
-    emails_in_quarantine = Quarantine.query.filter(Quarantine.fk_user == user.id).with_entities(Quarantine.email_id).all()
-    emails_in_quarantine = [mail[0] for mail in emails_in_quarantine]
-
-    whitelist_expressions = WhiteListRegularExpression.query\
-        .filter(WhiteListRegularExpression.fk_user == user.id)\
-        .with_entities(WhiteListRegularExpression.expression).all()
-    whitelist_expressions = [mail[0] for mail in whitelist_expressions]
-
-    blacklist_expressions = BlackListRegularExpression.query\
-        .filter(BlackListRegularExpression.fk_user == user.id)\
-        .with_entities(BlackListRegularExpression.expression).all()
-    blacklist_expressions = [mail[0] for mail in blacklist_expressions]
-
-    black_list = BlackList.query.filter(BlackList.fk_user == user.id).all()
-    black_list = [mail.email for mail in black_list]
-
-    parameters = {'PERSON_NAME': user.full_name.title()}
-    for mail in mails:
-        sender = mailbox.get_sender(mail)
-        sender = sender.strip('>').split('<')[-1]
-        if sender in white_list:
-            if is_unseen:
-                # We mark the email as unseen since the user has not read it yet
-                mailbox.mark_as_unseen(mail)
-        elif sender in black_list:
-            # We delete the email
-            # mailbox.delete(mail)
-            print("Deleting")
-        elif fulfils_expression(sender, whitelist_expressions):
-            if is_unseen:
-                # We mark the email as unseen since the user has not read it yet
-                mailbox.mark_as_unseen(mail)
-        elif fulfils_expression(sender, blacklist_expressions):
-            # We delete the email
-            # mailbox.delete(mail)
-            print("Deleting")
-        else:
-            # we add the email to the quarantine
-            # We get all the content of the email
-            message = mailbox.get_mail(mail)
-            if message.message_id() in emails_in_quarantine:  # TODO: delete this if
-                print("(Provisional) Message already in quarantine")
-                # mailbox.mark_as_unseen(mail)
-                continue
-            path = os.path.join(BASE_PATH, user.email, message.message_id() + '.eml')
-            # we save the email in the repository
-            size = message.save(path)
-            logging.info('Message from {} with id {} and size {}, was deleted and saved in {}\n'.format(sender, message.message_id(), size, path))
-            # mailbox.delete(mail)
-            # we save some information in the database
-            quarantined_email = Quarantine(fk_user=user.id, email_sender=sender, email_subject=message.subject(),
-                                           email_size=size, email_id=message.message_id())
-            db.session.add(quarantined_email)
-            db.session.commit()
-            # we send an email with the captcha
-            smtp_sender.send_message(user.email, sender, 'RE: ' + message.subject(),
-                                     MessageCreator.create_message_template(TEMPLATE_NAME, parameters))
 
 
 def get_imap_server(user_email):
@@ -135,8 +58,6 @@ def main():
     if not smtp_sender.login(user_email, password):
         return
     mailbox.select('inbox')
-    white_list = WhiteList.query.filter(WhiteList.fk_user == user.id).all()
-    white_list = [mail.email for mail in white_list]
 
     last_uid = user.last_uid_scanned
     if last_uid == 0:
@@ -153,17 +74,17 @@ def main():
     if seen_emails[0::-1] == [str(user.last_uid_scanned)]:
         seen_emails.pop(0)
 
-    analyse_mails(mailbox, smtp_sender, white_list, seen_emails, False, user)
-    analyse_mails(mailbox, smtp_sender, white_list, unseen_emails, True, user)
-    mails_scanned = len(unseen_emails) + len(seen_emails)
+    mails_to_scan = len(unseen_emails) + len(seen_emails)
 
-    if mails_scanned > 0:
+    if mails_to_scan > 0:
+        email_analyzer = EmailAnalyzer(mailbox, smtp_sender, user)
+        email_analyzer.analyse_mails(seen_emails, False)
+        email_analyzer.analyse_mails(unseen_emails, True)
+
         # we get the greatest uid scanned
         last_scanned_id = max([int(i) for i in (unseen_emails[-1:] or [0]) + (seen_emails[-1:] or [0])])
         user.last_uid_scanned = last_scanned_id
         db.session.commit()
-    # user.last_uid_scanned += mails_scanned
-    # db.session.commit()
 
     # We restore the emails that need to be restored
     restore_emails(mailbox, user)
---
+++ spam/email_analyzer.py
@@ -0,0 +1,94 @@
+import re
+import os
+from spam import db
+from spam.models import WhiteList, BlackList, WhiteListRegularExpression, BlackListRegularExpression, Quarantine
+from spam.message_creator import MessageCreator
+from dotenv import load_dotenv
+load_dotenv()
+
+BASE_PATH = os.environ.get("BASE_PATH")
+TEMPLATE_NAME = 'captcha_email.html'
+
+
+def fulfils_expression(mail, expressions):
+    """It returns True if the email address matches one of the regular expressions in the expressions list"""
+    for expression in expressions:
+        if re.fullmatch(expression, mail):
+            return True
+    return False
+
+
+class EmailAnalyzer:
+    def __init__(self, mailbox, smtp_sender, user):
+        self.mailbox = mailbox
+        self.smtp_sender = smtp_sender
+        self.user = user
+
+        self.whitelist_expressions = WhiteListRegularExpression.query\
+            .filter(WhiteListRegularExpression.fk_user == user.id)\
+            .with_entities(WhiteListRegularExpression.expression).all()
+        self.whitelist_expressions = [mail[0] for mail in self.whitelist_expressions]
+
+        self.blacklist_expressions = BlackListRegularExpression.query\
+            .filter(BlackListRegularExpression.fk_user == user.id)\
+            .with_entities(BlackListRegularExpression.expression).all()
+        self.blacklist_expressions = [mail[0] for mail in self.blacklist_expressions]
+
+        self.white_list = WhiteList.query.filter(WhiteList.fk_user == user.id).all()
+        self.white_list = [mail.email for mail in self.white_list]
+
+        self.black_list = BlackList.query.filter(BlackList.fk_user == user.id).all()
+        self.black_list = [mail.email for mail in self.black_list]
+
+    def analyse_mails(self, mails, is_unseen):
+        """It checks the sender for each email from the list. If the sender is not in the white list it will do one of the
+        following actions:
+            - If the email is in the black list, it will delete it from the mailbox.
+            - If the email is not in the black list, it will delete it from the mailbox, save it in a repository as an .eml file,
+              save some specific data in the database and send an email to the sender with the captcha.
+        """
+        emails_in_quarantine = Quarantine.query.filter(Quarantine.fk_user == self.user.id).with_entities(Quarantine.email_id).all()
+        emails_in_quarantine = [mail[0] for mail in emails_in_quarantine]
+
+        parameters = {'PERSON_NAME': self.user.full_name.title()}
+        for mail in mails:
+            sender = self.mailbox.get_sender(mail)
+            sender = sender.strip('>').split('<')[-1]
+            if sender in self.white_list:
+                if is_unseen:
+                    # We mark the email as unseen since the user has not read it yet
+                    self.mailbox.mark_as_unseen(mail)
+            elif sender in self.black_list:
+                # We delete the email
+                # mailbox.delete(mail)
+                print("Deleting")
+            elif fulfils_expression(sender, self.whitelist_expressions):
+                if is_unseen:
+                    # We mark the email as unseen since the user has not read it yet
+                    self.mailbox.mark_as_unseen(mail)
+            elif fulfils_expression(sender, self.blacklist_expressions):
+                # We delete the email
+                # mailbox.delete(mail)
+                print("Deleting")
+            else:
+                # we add the email to the quarantine
+                # We get all the content of the email
+                message = self.mailbox.get_mail(mail)
+                if message.message_id() in emails_in_quarantine:  # TODO: delete this if
+                    print("(Provisional) Message already in quarantine")
+                    # mailbox.mark_as_unseen(mail)
+                    continue
+                path = os.path.join(BASE_PATH, self.user.email, message.message_id() + '.eml')
+
+                # we save the email in the repository
+                size = message.save(path)
+                print('Message from {} with id {} and size {}, was deleted and saved in {}\n'.format(sender, message.message_id(), size, path))
+                # mailbox.delete(mail)
+                # we save some information in the database
+                quarantined_email = Quarantine(fk_user=self.user.id, email_sender=sender, email_subject=message.subject(),
+                                               email_size=size, email_id=message.message_id())
+                db.session.add(quarantined_email)
+                db.session.commit()
+                # we send an email with the captcha
+                self.smtp_sender.send_message(self.user.email, sender, 'RE: ' + message.subject(),
+                                              MessageCreator.create_message_template(TEMPLATE_NAME, parameters))

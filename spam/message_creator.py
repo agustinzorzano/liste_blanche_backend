ef804a6ce9fcf8e25027e65042faeacb11596688
agustin <aguszorza@gmail.com>
2021-03-07 20:23:45 +0100

    license

--- spam/message_creator.py
+++ spam/message_creator.py
@@ -1,3 +1,22 @@
+"""
+Copyright 2021 Venceslas Roullier, Daniel Santos Bustos, Guillem Sanyas, Julien Tagnani, Agustin Zorzano
+
+This file is part of OpenEmailAntispam.
+
+    OpenEmailAntispam is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenEmailAntispam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenEmailAntispam.  If not, see https://www.gnu.org/licenses/.
+"""
+
 from jinja2 import Environment, FileSystemLoader
 import os
 
